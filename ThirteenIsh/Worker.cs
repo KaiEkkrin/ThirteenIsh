557c17c43cf18edf8756632ab431c9bcdf4a601d
Alex Holloway <alex.holloway.ctx@gmail.com>
2024-01-22 22:36:57 +0000

    Clean up template

--- ThirteenIsh/Worker.cs
+++ ThirteenIsh/Worker.cs
@@ -1,24 +1,19 @@
-namespace ThirteenIsh
+namespace ThirteenIsh;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
 {
-    public class Worker : BackgroundService
+    private static readonly Action<ILogger, DateTimeOffset, Exception?> WorkerRunningMessage =
+        LoggerMessage.Define<DateTimeOffset>(
+            LogLevel.Information,
+            new EventId(1, nameof(Worker)),
+            "Worker running at: {Time}");
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
-            }
+            WorkerRunningMessage(logger, DateTimeOffset.Now, null);
+            await Task.Delay(1000, stoppingToken);
         }
     }
 }
