57449ccfb294f3cbcfb7929cd90e4c0b204f96a2
Alex Holloway <alex.holloway.ctx@gmail.com>
2024-04-11 21:59:21 +0100

    Suppress migration warnings

---
+++ ThirteenIsh.Database/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Auto-generated migration code", Scope = "namespaceanddescendants", Target = "~N:ThirteenIsh.Database.Migrations")]
+[assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "Auto-generated migration code", Scope = "namespaceanddescendants", Target = "~N:ThirteenIsh.Database.Migrations")]
