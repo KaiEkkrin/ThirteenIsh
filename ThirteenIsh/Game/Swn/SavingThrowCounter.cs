6706f6fbe6bf4ca4ae70b0c07fae234f95d26a9f
Alex Holloway <alex.holloway.ctx@gmail.com>
2025-02-23 16:36:27 +0000

    Roll saving throws

--- ThirteenIsh/Game/Swn/SavingThrowCounter.cs
+++ ThirteenIsh/Game/Swn/SavingThrowCounter.cs
@@ -1,4 +1,5 @@
 ﻿using ThirteenIsh.Database.Entities;
+using ThirteenIsh.Parsing;
 
 namespace ThirteenIsh.Game.Swn;
 
@@ -15,6 +16,27 @@ internal class SavingThrowCounter(string name, params AttributeBonusCounter[] at
         return GetSavingThrow(attributeBonuses.Select(a => a.GetValue(character.Sheet)));
     }
 
+    public override GameCounterRollResult Roll(
+        ITrackedCharacter character, ParseTreeBase? bonus, IRandomWrapper random, int rerolls, ref int? targetValue)
+    {
+        ParseTreeBase parseTree = DiceRollParseTree.BuildWithRerolls(20, rerolls);
+        if (bonus is not null)
+        {
+            parseTree = new BinaryOperationParseTree(0, parseTree, bonus, '+');
+        }
+
+        var rolledValue = parseTree.Evaluate(random, out var working);
+        targetValue ??= GetValue(character);
+        return new GameCounterRollResult
+        {
+            CounterName = Name,
+            Error = GameCounterRollError.Success,
+            Roll = rolledValue,
+            Working = working,
+            Success = targetValue.HasValue ? rolledValue >= targetValue : null
+        };
+    }
+
     private static int? GetSavingThrow(IEnumerable<int?> bonuses)
     {
         // The saving throw is 15 minus the higher of the two bonuses
@@ -25,6 +47,6 @@ internal class SavingThrowCounter(string name, params AttributeBonusCounter[] at
         }
 
         int? savingThrowValue = bonusValues.Count == 0 ? null : bonusValues.Max();
-        return savingThrowValue;
+        return 15 - savingThrowValue;
     }
 }
